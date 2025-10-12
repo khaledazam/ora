77b792ceb585e0496845b39b3c375e2470e44910
khaledazam <khaled141ma@gmail.com>
2025-10-12 13:28:44 +0300

    project setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from '@/components/ui/card'
+import { SignedIn, SignedOut, SignOutButton, SignUpButton } from '@clerk/nextjs'
+import React from 'react'
+
+const aboutPage = () => {
+  return (
+    <div>
+      <h1>about page</h1>
+      </div>
+  )
+}
+
+export default aboutPage
